cfa7d5dbc8ff26ee0584e28ed1c7099a8fef4373
Zoey Wen <zoey.wen@synaptop.com>
2018-02-11 20:50:49 -0500

    base

---
+++ app/home/home.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {StateService} from '../common/state.service';
+
+@Component({
+  selector: 'home',
+  template: require('./home.component.html')
+})
+export class HomeComponent implements OnInit {
+  title: string = 'Wedding Plan Checklist';
+  subTitle:  string = 'Plan the wedding yourslef and manage your budget.';
+  message: string;
+
+  constructor(private _stateService: StateService) { }
+
+  ngOnInit() {
+    this.message = this._stateService.getMessage();
+  }
+
+  updateMessage(m: string): void {
+    this._stateService.setMessage(m);
+  }
+}
